ec46913728d56f034019142c3c0648240daae499
Tom Runia <tomrunia@gmail.com>
2018-03-05 21:16:47 +0100

    fix

--- train.py
+++ train.py
@@ -147,7 +147,7 @@ def validate(net, criterion, data_loader):
         print("[{}] Performing validation {:04d}/{:04d}, Examples/Sec = {:.2f}, "
               "Accuracy = {:.3f}, Loss = {:.3f}".format(
             datetime.now().strftime("%A %H:%M"), valid_step, num_batches,
-            examples_per_second.average(), loss, acc
+            examples_per_second.average(), loss.data[0], acc
         ))
 
         # Save one validation example from the first batch
