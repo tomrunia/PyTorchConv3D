eb047cfdb5b5d6a76d236fb30ddf922a31ab6f37
Tom Runia <tomrunia@gmail.com>
2018-03-06 17:39:25 +0100

    test model performance

--- test.py
+++ test.py
@@ -16,77 +16,158 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import os
-import shutil
-import json
+
 import time
 from datetime import datetime
 import argparse
 
 import numpy as np
+import cv2
 
 import torch
 from torch import nn
 from torch import optim
+from torch.optim.lr_scheduler import StepLR
 from torch.autograd import Variable
-import torch.utils.data
 import torchvision.utils
+
+import torch.utils.data
 import torchvision.transforms
+from torch.utils.data.sampler import SubsetRandomSampler
+from video_transform.spatial import *
+
+from dataset import BlenderSyntheticDataset
 
 from models.conv3d import Conv3D_Repetition
-from video_transform.spatial import *
-from dataset import BlenderSyntheticDataset
-from utils import AverageMeter, calculate_accuracy
-
-from tensorboardX import SummaryWriter
+from dataset import init_datasets
+from utils import *
 
 ################################################################################
-
 
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description='Train 3D ConvNet', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
     # Positional arguments
-    parser.add_argument('--data_path', type=str, help='Root path for dataset.')
-    parser.add_argument('--checkpoint_path', type=str, default='./output/', help='Root path for dataset.')
-    parser.add_argument('--batch_size', type=int, default=10, help='Batch size.')
+    parser.add_argument('--data_path', type=str, required=True, help='Root path for dataset.')
+    parser.add_argument('--checkpoint_file', type=str, required=True, help='Root path for dataset.')
+
+    # Optimization options
+    parser.add_argument('--epochs', type=int, default=50, help='Number of epochs to train.')
+    parser.add_argument('--batch_size', type=int, default=64, help='Batch size.')
+    parser.add_argument('--valid_frac', type=float, default=0.1, help='Fraction of dataset to use for validation.')
+    parser.add_argument('--inspect', type=bool, default=False, help='Whether to inspect classification results.')
 
     # Acceleration
-    parser.add_argument('--ngpu', type=int, default=1, help='0 = CPU.')
-    parser.add_argument('--workers', type=int, default=4, help='Pre-fetching threads.')
+    parser.add_argument('--num_gpu', type=int, default=1, help='Number of GPUs. Set to 0 to perform on CPU.')
+    parser.add_argument('--workers', type=int, default=8, help='Pre-fetching threads.')
 
     args = parser.parse_args()
 
     ############################################################################
 
-    # Define the input pipeline
-    spatial_transform = Compose([
+    valid_transform = Compose([
+        torchvision.transforms.ToPILImage(),
         ToTensor(225),
         Normalize([0, 0, 0], [1, 1, 1])
     ])
 
-    dataset = BlenderSyntheticDataset(dataset_path=args.data_path, spatial_transform=spatial_transform)
-    data_loader = torch.utils.data.DataLoader(
-        dataset, batch_size=args.batch_size, shuffle=False,
+    valid_data = BlenderSyntheticDataset(args.data_path, spatial_transform=valid_transform)
+
+    # Determine which examples to use for training and validation
+    num_examples = len(valid_data)
+    indices = np.arange(0, num_examples)
+    split = int(np.floor(args.valid_frac * num_examples))
+
+    # Set the indices for training and validation
+    valid_idx = indices[:split]
+    valid_sampler = SubsetRandomSampler(valid_idx)
+
+    valid_loader = torch.utils.data.DataLoader(
+        dataset=valid_data, batch_size=args.batch_size, sampler=valid_sampler,
         num_workers=args.workers, pin_memory=True)
 
-    # Define the network
-    net = Conv3D_Repetition(num_classes=dataset.num_classes())
-    if args.ngpu > 0: net.cuda()
+    ############################################################################
 
-    # This has any effect only on modules such as Dropout or BatchNorm.
+    # Define the network
+    net = Conv3D_Repetition(num_classes=valid_data.num_classes())
+
+    print("Restoring weights from checkpoint: {}".format(args.checkpoint_file))
+    assert os.path.exists(args.checkpoint_file), "Checkpoint does not exist."
+
+    checkpoint = torch.load(args.checkpoint_file)
+    net.load_state_dict(checkpoint['state_dict'])
+    print("Succesfully restored checkpoint.")
+
+    if args.num_gpu > 0:
+        net.cuda()
+
     net.eval()
 
-    # for epoch in range(args.epochs):
-    #
-    #     # Perform optimization for one epoch
-    #     train_epoch(epoch=epoch, net=net, data_loader=train_loader, optimizer=optimizer,
-    #                 summary_writer=summary_writer,
-    #                 scalar_summary_interval=args.scalar_summary_interval,
-    #                 image_summary_interval=args.image_summary_interval,
-    #                 checkpoint_path=checkpoint_path, checkpoint_interval=args.checkpoint_interval)
-    #
-    #
+    # Loss criterion and optimizer
+    criterion = nn.CrossEntropyLoss()
+
+    ############################################################################
+
+    num_batches = len(valid_loader)
+    batch_losses = np.zeros(num_batches)
+    batch_accuracies = np.zeros(num_batches)
+
+    for i, (inputs, labels) in enumerate(valid_loader):
+
+        inputs = Variable(inputs)
+        labels = Variable(labels)
+        if args.num_gpu > 0:
+            inputs = inputs.cuda()
+            labels = labels.cuda()
+
+        # Forward pass through the network
+        logits = net(inputs)
+
+        # Calculate and save metrics
+        loss = criterion(logits, labels)
+        acc = calculate_accuracy(logits, labels)
+
+        batch_losses[i] = loss.data[0]
+        batch_accuracies[i] = acc
+
+        print("[{}] Performing validation {:04d}/{:04d}, "
+              "Accuracy = {:.3f}, Loss = {:.3f}".format(
+            datetime.now().strftime("%A %H:%M"), i, num_batches,
+            acc, loss.data[0]
+        ))
+
+        ########################################################################
+        # NumPy inspection of what examples are misclassified
+
+        if args.inspect == True:
+
+            videos      = np.squeeze(inputs.data.cpu().numpy(), axis=1)
+            targets     = labels.data.cpu().numpy()
+            predictions = np.argmax(logits.data.cpu().numpy(), axis=1)
+
+            for example_idx in range(args.batch_size):
+                correct = (targets[example_idx] == predictions[example_idx])
+                print("Example {}. Target = {}. Prediction = {}. Correct = {}".format(
+                    example_idx, targets[example_idx], predictions[example_idx], correct
+                ))
+                for frame_idx in range(videos.shape[1]):
+                    frame_norm = np.zeros_like(videos[example_idx,frame_idx,:])
+                    cv2.normalize(videos[example_idx,frame_idx,], frame_norm, 0.0, 1.0, cv2.NORM_MINMAX)
+                    cv2.imshow("Frame", frame_norm)
+                    key = cv2.waitKey(0)
+                    if key == ord('n'):
+                        break
+
+        ########################################################################
+
+    total_loss = float(np.mean(batch_losses))
+    total_acc  = float(np.mean(batch_accuracies))
+
+    print("VALIDATION SUMMARY ({} batches):".format(len(valid_loader)))
+    print("  Loss:     {:.3f}".format(total_loss))
+    print("  Accuracy: {:.3f}".format(total_acc))
+    print("#"*60)
+
 
 
